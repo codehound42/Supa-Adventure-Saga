1ec821ec137ba1e7d907e32fd4180a8b92499427
codehound42 <codehound42@gmail.com>
2023-12-11 13:09:51 +0100

    Init streamlit openai chatbot app

---
+++ app.py
@@ -0,0 +1,32 @@
+from openai import OpenAI
+import streamlit as st
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("💬 Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    print("msg", msg)
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+    client = OpenAI(api_key=openai_api_key)
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    st.chat_message("")
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages, stream=False)
+    msg = response.choices[0].message.model_dump()
+    msg = {"role": msg["role"], "content": msg["content"]}
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg["content"])
