eebe2e7c4a209249c6a2fbbeaaf62a1466f18e02
codehound42 <codehound42@gmail.com>
2023-12-11 17:27:27 +0100

    D&D creation prompting

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from dnd import character_system_msg, CharacterNotebook
+from langchain.utils.openai_functions import convert_pydantic_to_openai_function
+
 
 msgs = StreamlitChatMessageHistory(key="messages")
 
-memory = ConversationBufferMemory(
-    memory_key="history", chat_memory=msgs, return_messages=True
-)
-st.write(memory.load_memory_variables({}))
+memory = ConversationBufferMemory(memory_key="history", chat_memory=msgs, return_messages=True)
 
 with st.sidebar:
     openai_api_key = st.text_input(
@@ -23,7 +23,7 @@ with st.sidebar:
 st.title("💬 Chatbot")
 
 if len(msgs.messages) == 0:
-    msgs.add_ai_message("How can I help you?")
+    msgs.add_ai_message("Hello! Are you ready to create your character for the game of Dungeons and Dragons?")
 
 for msg in msgs.messages:
     st.chat_message(msg.type).write(msg.content)
@@ -33,20 +33,24 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    chat = ChatOpenAI(api_key=st.session_state.openai_api_key)
+    llm = ChatOpenAI(api_key=st.session_state.openai_api_key)
+    character_model = llm.bind(
+        functions=[convert_pydantic_to_openai_function(CharacterNotebook)],
+    )
     prompt_chat = ChatPromptTemplate.from_messages(
         [
-            ("system", "You are an AI chatbot having a conversation with a human."),
+            ("system", character_system_msg),
             MessagesPlaceholder(variable_name="history"),
             ("human", "{prompt}"),
         ]
     )
     llm_chain = LLMChain(
-        llm=chat,
+        llm=character_model,
         memory=memory,
         prompt=prompt_chat,
     )
 
     st.chat_message("human").write(prompt)
     response = llm_chain({"prompt": prompt})
+    st.write("response", response)
     st.chat_message("ai").write(response["text"])
--- dnd.py
+++ dnd.py
@@ -1,12 +1,4 @@
-import json
-
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.pydantic_v1 import BaseModel, Field
-from langchain.utils.openai_functions import convert_pydantic_to_openai_function
-from langchain_core.language_models import BaseChatModel
-from langchain_core.messages import AIMessage, AnyMessage, HumanMessage
-from permchain import BaseCheckpointAdapter, Channel, Pregel
-from permchain.channels import LastValue, Topic
 
 character_system_msg = """You are a dungeon master for a game of dungeons and dragons.
 
@@ -25,25 +17,4 @@ Once you have gathered enough information, write that info to `notebook`."""
 class CharacterNotebook(BaseModel):
     """Notebook to write information to"""
 
-    player_info: str = Field(
-        description="Information about a player that you will remember over time"
-    )
-
-
-character_prompt = ChatPromptTemplate.from_messages(
-    [("system", character_system_msg), MessagesPlaceholder(variable_name="messages")]
-)
-
-def create_dnd_bot(llm: BaseChatModel, checkpoint: BaseCheckpointAdapter):
-    character_model = llm.bind(
-        functions=[convert_pydantic_to_openai_function(CharacterNotebook)],
-    )
-
-character_chain = (
-    character_prompt
-    | character_model
-    | Channel.write_to("messages")
-)
-
-if __name__ == "__main__":
-    character_chain.invoke()
+    player_info: str = Field(description="Information about a player that you will remember over time")
